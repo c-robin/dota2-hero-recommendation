0e0062eb63d7c5521aed705cb0ec225b077a5e1a
Charles Robin <charles.am.robin@gmail.com>
2015-07-22 22:40:10 +0200

    Add unit test for tasks

--- recommendation/tasks.py
+++ recommendation/tasks.py
@@ -22,11 +22,15 @@ def update_hero():
         exec(code, config)
     api_key = config['API_KEY']
     url = 'https://api.steampowered.com/IEconDOTA2_570/GetHeroes/v0001/?key='+ api_key+'&language=en_us'
-    r = requests.get(url, timeout=None, proxies = None)
-    json_heroes = json.loads(r.text)
+    json_heroes = json.loads(get_json(url))
     
     for hero in json_heroes['result']['heroes']:
         try:
             Hero.objects.get(dota2_hero_id=hero['id'])
         except ObjectDoesNotExist:
             Hero.objects.create(dota2_hero_id=hero['id'], name=hero['localized_name'])
+            
+def get_json(url):
+    r = requests.get(url, timeout=None, proxies = None)
+    return r.text
+    
---
+++ recommendation/teststasks.py
@@ -0,0 +1,32 @@
+from django.test import TestCase
+from django.test.utils import override_settings
+from recommendation.tasks import update_hero
+
+from recommendation.models import Hero
+from recommendation.models import Duel
+from recommendation.models import Team
+from recommendation.models import Match
+
+import responses
+
+class TasksTestCase(TestCase):
+
+    def setUp(self):
+        pass
+     
+    @override_settings(CELERY_ALWAYS_EAGER=True, TEST_RUNNER = 'djcelery.contrib.test_runner.CeleryTestSuiteRunner')
+    def testExample(self):
+        config = {}
+        with open("config.py") as f:
+            code = compile(f.read(), "config.py", 'exec')
+            exec(code, config)
+        api_key = config['API_KEY']   
+        responses.add(**{
+            'method'         : responses.GET,
+            'url'            : 'https://api.steampowered.com/IEconDOTA2_570/GetHeroes/v0001/?key='+ api_key+'&language=en_us',
+            'body'           : '{"result": {"heroes": [{"name": "npc_dota_hero_antimage","id": 1,"localized_name": "Anti-Mage"},{"name": "npc_dota_hero_axe","id": 2,"localized_name": "Axe"}]}},',
+            'status'         : 200,
+            'content_type'   : 'application/json'
+        })
+        update_hero.delay()
+        self.assertEqual(str(Hero.objects.get(dota2_hero_id="1")), "Anti-Mage")
