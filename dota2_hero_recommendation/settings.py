3fdd3a93008cf2f364d7fddf946b5f70ba70099f
Charles Robin <charles.am.robin@gmail.com>
2015-07-22 23:04:48 +0200

    Fix tests

--- dota2_hero_recommendation/settings.py
+++ dota2_hero_recommendation/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
+import sys
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -137,3 +138,6 @@ LOGGING = {
         },
     }
 }
+
+if 'test' in sys.argv:
+    CELERY_ALWAYS_EAGER = True
