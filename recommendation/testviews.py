b43af66bffec4b9855f41ababdd94f4c9e15f3c4
Charles Robin <charles.am.robin@gmail.com>
2015-07-27 22:24:23 +0200

    Add UT for Home view

--- recommendation/testviews.py
+++ recommendation/testviews.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from django.test.utils import override_settings
 from recommendation.views import update_hero
+from recommendation.views import home
 from . import tasks
 from mock import Mock, patch
 
@@ -10,5 +11,12 @@ class ViewsTestCase(TestCase):
         request = ''
         with patch('recommendation.tasks.update_hero.delay') as patch_mock:
             response = update_hero(request)
+            self.assertTrue(patch_mock.called)
+        self.assertEqual(response.status_code, 200)
+        
+    def testHome(self):
+        request = ''
+        with patch('recommendation.tasks.logger.delay') as patch_mock:
+            response = home(request)
             self.assertTrue(patch_mock.called)
         self.assertEqual(response.status_code, 200)
